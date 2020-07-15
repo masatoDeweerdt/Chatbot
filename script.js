25ff9167aa60179507b38614e3bdbb39566d29a1
MeunierS <meuniersebastien1987@gmail.com>
2020-07-15 16:04:01 +0200

    cas oui et non terminé

--- script.js
+++ script.js
@@ -11,52 +11,121 @@ init.appendChild(textnode); // met le text en child de <p>
 init.setAttribute('class', "bot");
 targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
 let counterQuestion = 0;
-let retour = 0;
 
 function getRandomInt(max) { // fonction génère un entier aléatoire
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function test(reponse) {
+function test(reponse) { // test de la réponse si oui ou non ou autre
 
     if (reponse === "yes") {
         counterQuestion++;
-        retour++;
     } else if (reponse === "no") {
-        counterQuestion++;
+        counterQuestion += 3;
     } else {
 
     }
 }
 
-function annif(reponse) {
+function annif(reponse) { //1ère question si oui
+    let randomNum = getRandomInt(3);
+    let quest = ["Is it your birthday today ?", "Is today the date remembering your birth ?",
+        "Can it be your birthday today ?"
+    ];
+    let init = document.createElement("p"); //création de l'élément <p>
+    let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+    let textnode = document.createTextNode(astroBot + quest[randomNum]); // crée le string Vous : + input de l'user
+    init.appendChild(textnode); // met le text en child de <p>
+    init.setAttribute('class', "bot");
+    targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
 
+}
+
+function finish(reponse) { //réponse à 1ère question
     if (reponse === "yes") {
-        let randomNum = getRandomInt(3);
-        let quest = ["Is it your birthday today ?", "Is today the date remembering your birth ?",
-            "Can it be your birthday today ?"
-        ];
+
         let init = document.createElement("p"); //création de l'élément <p>
         let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
-        let textnode = document.createTextNode(astroBot + quest[randomNum]); // crée le string Vous : + input de l'user
+        let textnode = document.createTextNode(astroBot + "Happy Birthday to you !"); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
+        init = document.createElement("p"); //création de l'élément <p>
+        targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        textnode = document.createTextNode(astroBot + "Go do a party, See you next time."); // crée le string Vous : + input de l'user
         init.appendChild(textnode); // met le text en child de <p>
         init.setAttribute('class', "bot");
         targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
 
     } else if (reponse === "no") {
 
+        let init = document.createElement("p"); //création de l'élément <p>
+        let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        let textnode = document.createTextNode(astroBot + "What a shame."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
+        init = document.createElement("p"); //création de l'élément <p>
+        targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        textnode = document.createTextNode(astroBot + "Come back see to me when it's your birthday."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
     } else {
 
     }
 }
 
-function crime(reponse) {
+function crime(reponse) { //2e question si non
+    counterQuestion+=2;
+    let randomNum = getRandomInt(3);
+    let quest = ["Have you commited a crime ?", "Are you a criminal ?",
+        "Are you on the wrong side of justice ?"
+    ];
+    let init = document.createElement("p"); //création de l'élément <p>
+    let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+    let textnode = document.createTextNode(astroBot + quest[randomNum]); // crée le string Vous : + input de l'user
+    init.appendChild(textnode); // met le text en child de <p>
+    init.setAttribute('class', "bot");
+    targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
 
+}
+function finish2(reponse) { //réponse 2e question
     if (reponse === "yes") {
-        counterQuestion++;
-        
+
+        let init = document.createElement("p"); //création de l'élément <p>
+        let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        let textnode = document.createTextNode(astroBot + "Unbelievable, please don't move anywhere."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
+        init = document.createElement("p"); //création de l'élément <p>
+        targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        textnode = document.createTextNode(astroBot + "Astro Bot is calling the police."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
     } else if (reponse === "no") {
-        counterQuestion++;
+
+        let init = document.createElement("p"); //création de l'élément <p>
+        let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        let textnode = document.createTextNode(astroBot + "That's good to hear."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
+        init = document.createElement("p"); //création de l'élément <p>
+        targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+        textnode = document.createTextNode(astroBot + "Im proud of you, continue like that."); // crée le string Vous : + input de l'user
+        init.appendChild(textnode); // met le text en child de <p>
+        init.setAttribute('class', "bot");
+        targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
+
     } else {
 
     }
@@ -65,26 +134,37 @@ function crime(reponse) {
 document.getElementById("send").addEventListener("click", () => {
     let node = document.createElement("p"); //création de l'élément <p>
     let target = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
-    reponse1 = document.getElementById("inputBox").value;
-    let textnode = document.createTextNode(user + reponse1); // crée le string Vous : + input de l'user
+    let reponse = document.getElementById("inputBox").value;
+    let textnode = document.createTextNode(user + reponse); // crée le string Vous : + input de l'user
     node.appendChild(textnode); // met le text en child de <p>
     node.setAttribute('class', "user");
     target.appendChild(node); //met le <p> en child dans la div ChatBox
 
     test(reponse);
+    console.log(counterQuestion);
 
-    if ((counterQuestion = 1) && (retour = 1)) {
-        annif(reponse);
-    } else if (counterQuestion = 1) {
-        crime(reponse);
-    } else if ((counterQuestion = 2) && (retour = 2)){
-
+    switch (counterQuestion) {
+        case 1:
+            annif(reponse);
+            break;
+        case 2:
+            finish(reponse);
+            break;
+        case 3:
+            crime(reponse);
+            break;
+        case 4:
+            finish(reponse);
+            break;
+        case 6:
+            finish2(reponse);
+            break;
+        case 8:
+            finish2(reponse);
+            break;
+        default :
+            break;
     }
-    else if (counterQuestion = 2){
 
-    }
-    else {
-
-    }
 
 });
