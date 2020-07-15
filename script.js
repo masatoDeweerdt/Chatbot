f880c6ad25bf65919b80b7fee12cfdf05a9e76bd
MeunierS <meuniersebastien1987@gmail.com>
2020-07-15 12:33:42 +0200

    text init d'Astro Bot

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const astroBot = "Astro Bot :";
-const user = "Vous :";
+const astroBot = "Astro Bot : ";
+const user = "Vous : ";
+
+let init = document.createElement("p"); //création de l'élément <p>
+let targetInit = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+let textnode = document.createTextNode(astroBot + "Hello, Im Astro Bot, are you fine today ?"); // crée le string Vous : + input de l'user
+init.appendChild(textnode); // met le text en child de <p>
+targetInit.appendChild(init); //met le <p> en child dans la div ChatBox
 
 
 document.getElementById("send").addEventListener("click", () => {
-    let node = document.createElement("p");
-    let target = document.getElementsByClassName("chatBox")[0];
-    let textnode = document.createTextNode(document.getElementById("inputBox").value);
-    console.log (node);
-    console.log (target);
-    console.log (textnode);
-    node.appendChild(textnode);
-    target.appendChild(node);
+    let node = document.createElement("p"); //création de l'élément <p>
+    let target = document.getElementsByClassName("chatBox")[0]; //défini la cible chatBox
+    let textnode = document.createTextNode(user + document.getElementById("inputBox").value); // crée le string Vous : + input de l'user
+    node.appendChild(textnode); // met le text en child de <p>
+    target.appendChild(node); //met le <p> en child dans la div ChatBox
+
+
 });
